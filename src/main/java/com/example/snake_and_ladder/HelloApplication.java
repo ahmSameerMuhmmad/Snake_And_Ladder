01f55548c60c490a508f3c7a320e2467f044d05b
ahmSameerMuhmmad <muhmmadsameer143@gmail.com>
2022-12-20 12:15:28 +0530

    Initial commit

---
+++ src/main/java/com/example/snake_and_ladder/HelloApplication.java
@@ -0,0 +1,29 @@
+package com.example.snake_and_ladder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    public static Group root;
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        root =new Group();
+        GamePage page=new GamePage();
+        root.getChildren().add(page.root);
+        stage.setTitle("Snake and Ladder");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
